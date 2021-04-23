f3dda765e394af96ac49af8ea5a7da00bc2f2b3c
michalmalinowski <michal.malinowski@greenparrot.pl>
2021-04-23 14:18:04 +0200

    Fix base locale checker

--- inc/Core/Base/Translations.php
+++ inc/Core/Base/Translations.php
@@ -17,7 +17,7 @@ class Translations extends BaseController
     public function load_my_transl()
     {
         $domain = 'develtio-forms';
-        $locale = apply_filters( 'plugin_locale', acf_get_locale(), $domain );
+        $locale = apply_filters( 'plugin_locale', get_locale(), $domain );
         $mofile = $domain . '-' . $locale . '.mo';
 
         load_textdomain( $domain, $this->plugin_path . 'lang/' . $mofile );
